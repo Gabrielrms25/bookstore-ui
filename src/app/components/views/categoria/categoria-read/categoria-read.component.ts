ab7e3d6b37e42cce914e116587488ee9cf79d000
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-08-09 20:16:42 -0300

    Ajustes no component-read

--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -10,8 +10,7 @@ import { Categoria } from '../categoria.model';
 export class CategoriaReadComponent implements OnInit {
   
   categorias: Categoria[] = []
-
-  displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+  displayedColumns: string[] = ['id', 'nome', 'descricao','livros','acoes'];
 
   constructor(private service: CategoriaService) { }
 
@@ -21,7 +20,7 @@ export class CategoriaReadComponent implements OnInit {
 
   findAll(){   
     this.service.findAll().subscribe(resposta => {
-      console.log(resposta)
+      //console.table(resposta)
       this.categorias = resposta;
     })
   }
