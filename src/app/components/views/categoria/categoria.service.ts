575edfac9de9c17217d7df201af63b9512ab8e6c
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-08-24 21:43:41 -0300

    Categoria DELETE implementada

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -21,11 +21,21 @@ export class CategoriaService {
     return this.http.get<Categoria[]>(url)
   }
 
+  findById(id:String): Observable<Categoria>{
+    const url = `${this.baseUrl}/categorias/${id}`
+    return this.http.get<Categoria>(url)
+  }
+
+  delete(id:String): Observable<void>{
+    const url = `${this.baseUrl}/categorias/${id}`
+    return this.http.delete<void>(url)
+  }
+
   create(categoria: Categoria): Observable<Categoria>{
     const url = `${this.baseUrl}/categorias`
     return this.http.post<Categoria>(url, categoria);
   }
- 
+
 
   mensagem(msg:String): void {
     this._snack.open(`${msg}`, 'OK',{
